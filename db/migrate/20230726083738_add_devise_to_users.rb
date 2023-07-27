8dd5fb5d1dcb2623497be0cf5f2485da67f53263
yacoubou-seidou <yacoseidou@gmail.com>
2023-07-27 11:34:57 +0100

    Make required changes Add email confirmation

--- db/migrate/20230726083738_add_devise_to_users.rb
+++ db/migrate/20230726083738_add_devise_to_users.rb
@@ -22,7 +22,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       # t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
+      t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_141135) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_27_103222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_141135) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "confirmation_token"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
