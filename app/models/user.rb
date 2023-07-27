66a32153e0b0b046aa149fb2dfe908dc7d1e7c60
yacoubou-seidou <yacoseidou@gmail.com>
2023-07-27 17:15:43 +0100

    Add the confirmable for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
