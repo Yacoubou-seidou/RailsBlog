a9379166dc75e449ca26729e2ccd2ed1931a5941
yacoubou-seidou <yacoseidou@gmail.com>
2023-07-28 11:34:58 +0100

    Fix file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,13 +40,4 @@ class PostsController < ApplicationController
       render :new
     end
   end
-
-  class Api::V1::PostsController < ApplicationController
-    def index
-      posts = User.find(params['user_id']).posts
-      render json: posts
-    rescue StandardError
-      render json: { error: 'User not found' }, status: :bad_request
-    end
-  end
 end
